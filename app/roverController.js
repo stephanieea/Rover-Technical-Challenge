cd48937beb23c64f66ca690a05e619010452fa9f
Stephanie Andrews <seandrews11@gmail.com>
2017-07-08 21:34:10 -0600

    Pass Rover Controller tests

--- app/roverController.js
+++ app/roverController.js
@@ -1,5 +1,24 @@
 class RoverController {
   constructor(plateau) {
     this.plateau = plateau
+    this.list = []
+  }
+
+  addRover(xPosition, yPosition, direction) {
+    this.list.push(new Rover(xPosition, yPosition, direction))
+  }
+
+  isRoverAtPosition(xCoordinate, yCoordinate) {
+    for (let i = 0; i < this.list.length; i++) {
+      if (this.list[i].xPosition === xCoordinate && this.list[i].yPosition === yCoordinate) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+
+  retrieveAllRoverPositions(){
+    return this.list.map(rover => `${rover.xPosition} ${rover.yPosition} ${rover.direction}`)
   }
 }
--- test/roverController-test.js
+++ test/roverController-test.js
@@ -12,7 +12,7 @@ describe('RoverController', () => {
   it('has a list for rovers', () => {
     const plateau = new Plateau(3, 3)
     const roverController = new RoverController(plateau)
-    assert.equal(roverController.list, [])
+    assert.deepEqual(roverController.list, [])
   })
 
   it('has a method to add rovers', () => {
@@ -24,8 +24,8 @@ describe('RoverController', () => {
   it('addRover updates list accurately', () => {
     const plateau = new Plateau(3, 3)
     const roverController = new RoverController(plateau)
-    assert.equal(roverController.list, [])
-    roverController.addRover('1 1 N')
+    assert.deepEqual(roverController.list, [])
+    roverController.addRover(1, 1, 'N')
     assert.equal(roverController.list.length, 1)
     assert.isObject(roverController.list[0])
     assert.equal(roverController.list[0].xPosition, 1)
@@ -34,17 +34,22 @@ describe('RoverController', () => {
   })
 
   it('has a method to see if any rovers are at a given plateau position', () => {
+    const plateau = new Plateau(3, 3)
+    const roverController = new RoverController(plateau)
     assert.isFunction(roverController.isRoverAtPosition)
   })
 
-  it('isRoverAtPosition returns true if there is one', () => {
+  it('isRoverAtPosition returns true or false as expected', () => {
     const plateau = new Plateau(3, 3)
     const roverController = new RoverController(plateau)
-    roverController.addRover('1 1 N')
-    assert.equal(roverController.isRoverAtPosition('1 1'), true)
+    roverController.addRover(1, 1, 'N')
+    assert.equal(roverController.isRoverAtPosition(1, 1), true)
+    assert.equal(roverController.isRoverAtPosition(2, 1), false)
   })
 
   it('has a method to retrieve all rover positions', () => {
+    const plateau = new Plateau(3, 3)
+    const roverController = new RoverController(plateau)
     assert.isFunction(roverController.retrieveAllRoverPositions)
   })
 
@@ -53,6 +58,8 @@ describe('RoverController', () => {
     const roverController = new RoverController(plateau)
     roverController.addRover('1 1 N')
     roverController.addRover('2 2 S')
-    assert.equal(roverController.retrieveAllRoverPositions(), ['1 1 N', '2 2 S'])
+    assert.equal(roverController.retrieveAllRoverPositions().length, 2)
+    assert.include(roverController.retrieveAllRoverPositions()[0], '1 1 N')
+    assert.include(roverController.retrieveAllRoverPositions()[1], '2 2 S')
   })
 })
