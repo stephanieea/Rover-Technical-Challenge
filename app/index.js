ba12cab4d23e1f9b4eeb87f474c52657393f4375
Stephanie Andrews <seandrews11@gmail.com>
2017-07-09 13:39:16 -0600

    Add html page for gui

--- app/index.js
+++ app/index.js
@@ -1,6 +1,1 @@
-var fs = require('fs')
-// read incoming file
-// create instances of platueau, controller, and rover(s)
-// pass controller directions
-// controller determines rover positions
-// print output file
+
